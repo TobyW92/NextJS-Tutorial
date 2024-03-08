d58f11ec2051cfc0d3d6f9abab53bf3dc5b4a55b
TobyW92 <tobywright@live.com>
2024-03-08 01:02:54 -0500

    completed chapter 16 + added customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+export default async function Page({
+    searchParams, }: {
+        searchParams?: {
+            query?: string;
+            page?: string;
+        };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const customers = await fetchFilteredCustomers(query);
+
+    // const customers = await fetchAllCustomers();
+    // console.log(customers);
+
+    return (
+        <div className="w-full">
+            {/* <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            </div> */}
+            <Table 
+                customers={customers}
+            />
+        </div>
+);
 }
